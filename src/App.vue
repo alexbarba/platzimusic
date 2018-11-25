a40bafe63937e27746117985e0fb87ec51f1b9dc
Alexis Barba <alexbarba.2278@gmail.com>
2018-11-25 14:10:08 -0700

    fixes por la URL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='https://alexbarba.github.io/platzimusic/dist/logo.png')
+    img(src='./assets/logo.png')
     h1 PlatziMusic
     select(v-model='selectedCountry')
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
